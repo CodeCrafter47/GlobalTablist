2ff331730d605d441602a4f060953125a449e90f
Florian Stober <Flo.e3@gmx.de>
2015-05-02 10:40:53 +0200

    add metrics

--- src/main/java/codecrafter47/globaltablist/GlobalTablist.java
+++ src/main/java/codecrafter47/globaltablist/GlobalTablist.java
@@ -99,6 +99,14 @@ public class GlobalTablist extends Plugin {
 
         ProxyServer.getInstance().getPluginManager().registerListener(this,
                 listener);
+
+        // Start metrics
+        try {
+            Metrics metrics = new Metrics(this);
+            metrics.start();
+        } catch (Throwable th) {
+            getLogger().log(Level.WARNING, "Failed to initialize Metrics", th);
+        }
     }
 
     /**
